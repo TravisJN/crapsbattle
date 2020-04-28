95342ba995de19e8d9320beaa4e3f3073314bb52
Travis Neufeld <tdizzletrav@gmail.com>
2020-04-27 21:14:53 -0700

    Add attack/defense types

--- src/data/Player.ts
+++ src/data/Player.ts
@@ -1,4 +1,4 @@
-import Die from "./DieModel";
+import Die, { eDiceType } from "./DieModel";
 import GameStateModel from './GameStateModel';
 
 export interface PlayerParams {
@@ -12,7 +12,7 @@ export default class Player {
     public maxHp: number;
     public rolledDice: Die[] = this.setNewDice();
 
-    private mNumberCount;  // keeps a count of each number rolled
+    private mNumberCount;
 
     constructor({ isHuman, maxHp }: PlayerParams) {
         this.isHuman = isHuman;
@@ -20,26 +20,26 @@ export default class Player {
     }
 
     public rollDice() {
-        this.mNumberCount = {
-            1: 0,
-            2: 0,
-            3: 0,
-            4: 0,
-            5: 0,
-            6: 0,
-        };
-
+        this.mNumberCount = this.getNumberCount();
         // First
         this.rolledDice = this.rolledDice.map((aDie: Die) => {
             if (!aDie.selected) {
                 aDie.number = this.getRandomInt();
+                aDie.type = (Math.random() > .5) ? 1 : 0;
             }
+            const dieType = this.mNumberCount[eDiceType[aDie.type]];
 
-            this.mNumberCount[aDie.number]++;
+            if (dieType[aDie.number]) {
+                dieType[aDie.number]++;
+            } else {
+                dieType[aDie.number] = 1;
+            }
 
             return aDie;
         }).map((aDie: Die) => {
-            aDie.multiplier = this.mNumberCount[aDie.number];
+            const dieType = this.mNumberCount[eDiceType[aDie.type]];
+
+            aDie.multiplier = dieType[aDie.number];
             return aDie;
         });
     }
@@ -47,11 +47,18 @@ export default class Player {
     public setNewDice(): Die[] {
         // Set all new dice to 1
         return Array.from({length: GameStateModel.NUM_DICE}, (e, index: number) => {
-            return new Die(1, index);
+            return new Die(1, index, eDiceType.NONE);
         });
     }
 
     private getRandomInt(min: number = 1, max: number = 6) {
         return Math.floor(Math.random() * Math.floor(max)) + min;
+    }
+
+    private getNumberCount() {
+        return {
+            ATTACK: {},
+            DEFENSE: {},
+        };
     }
 }
